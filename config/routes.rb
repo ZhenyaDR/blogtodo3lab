33664382fb5ac7cafa2a1d35c573a82df70632c1
ZhenyaDR <31968071+ZhenyaDR@users.noreply.github.com>
2017-09-18 17:02:34 +0300

    ok

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,8 @@ Rails.application.routes.draw do
   delete  'tododelete:id',  to: 'todo#destroy', as: 'tododelete'
   patch  'todoupdate:id',  to: 'todo#update', as: 'todoupdate'
   post   'todoedit:id',   to: 'todo#edit',     as: 'todoedit'
+  post   'updateone:id',   to: 'todo#updateone',     as: 'updateone'
+       
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
